81815d2dce80bf489220d59a09631a0e3dd3f742
Asus <lllllooooovveee@gmail.com>
2023-09-20 12:40:27 +0600

    dz3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tasks: [],
+            newTask: '',
+        }
+    }
+
+    addTask = () => {
+        const { newTask, tasks } = this.state;
+        if (newTask.trim() !== '') {
+            this.setState(prevState => ({
+                tasks: [...prevState.tasks, newTask],
+                newTask: '',
+            }));
+        }
+    }
+
+    render() {
+        const { tasks, newTask } = this.state;
+        return (
+            <div className="App">
+                <h1>to do list</h1>
+                <div>
+                    <input
+                        type="text"
+                        placeholder="Add task"
+                        value={newTask}
+                        onChange={(e) => this.setState({ newTask: e.target.value })}
+                    />
+                    <button onClick={this.addTask}>Add</button>
+                </div>
+                <ul>
+                    {tasks.map((task, index) => (
+                        <li key={index}>{task}</li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default App;
